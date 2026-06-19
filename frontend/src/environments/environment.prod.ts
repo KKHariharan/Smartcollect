6cf4b6779a0d7579a0ed1b38eebfea83d97c4115
KKHariharan <kathavarayanharihara@gmail.com>
2026-06-19 16:57:52 +0530

    eendpoint changed

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api/v1',
+  apiUrl: 'https://smartcollect-seven.vercel.app/api/v1',
   appName: 'SmartCollect',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:4000/api/v1',
-  apiUrl: 'https://smartcollect-seven.vercel.app/api/v1',
+  apiUrl: 'http://localhost:4000/api/v1',
+  // apiUrl: 'https://smartcollect-seven.vercel.app/api/v1',
   appName: 'SmartCollect',
 };
